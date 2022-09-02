a8e43540420effff80d2731e5d5b28484ff8129a
Ethan Salonga <ethansalonga28@gmail.com>
2022-09-02 14:24:06 -0400

    Build Nav component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Nav from "./components/Nav"
+
 function App() {
-  return <div className="App">Test</div>
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  )
 }
 
 export default App
