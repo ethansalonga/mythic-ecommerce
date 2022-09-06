0c5bd726c5b88ce5da56e16dac46093ae3b2cdb8
Ethan Salonga <ethansalonga28@gmail.com>
2022-09-06 00:13:55 -0400

    Build dynamic BookInfo pages

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,33 +1,23 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import Rating from "./Rating"
+import Price from "./Price"
 
 const Book = ({ book }) => {
   return (
     <div className="book">
-      <Link to="/books/1">
+      <Link to={`/books/${book.id}`}>
         <figure className="book__img--wrapper">
           <img src={book.url} alt="" className="book__img" />
         </figure>
       </Link>
       <div className="book__title">
-        <Link to="/books/1" className="book__title--link">
+        <Link to={`/books/${book.id}`} className="book__title--link">
           {book.title}
         </Link>
       </div>
       <Rating rating={book.rating} />
-      <div className="book__price">
-        {book.salePrice ? (
-          <>
-            <span className="book__price--normal">
-              ${book.originalPrice.toFixed(2)}
-            </span>{" "}
-            ${book.salePrice.toFixed(2)}
-          </>
-        ) : (
-          <>${book.originalPrice.toFixed(2)}</>
-        )}
-      </div>
+      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
     </div>
   )
 }
