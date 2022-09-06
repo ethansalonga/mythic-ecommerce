73105b9802900872ce7557e90475c1287cffd87a
Ethan Salonga <ethansalonga28@gmail.com>
2022-09-06 00:19:02 -0400

    Recommended books section

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom"
 import Rating from "../components/ui/Rating"
 import Price from "../components/ui/Price"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Book from "../components/ui/Book"
 
 const BookInfo = ({ books }) => {
   const { id } = useParams()
@@ -61,6 +62,14 @@ const BookInfo = ({ books }) => {
           <div className="row">
             <div className="book__selected--top">
               <h2 className="book__selected--title--top">Recommended Books</h2>
+            </div>
+            <div className="books">
+              {books
+                .filter(book => book.rating === 5 && +book.id !== +id)
+                .slice(0, 4)
+                .map(book => (
+                  <Book book={book} key={book.id} />
+                ))}
             </div>
           </div>
         </div>
