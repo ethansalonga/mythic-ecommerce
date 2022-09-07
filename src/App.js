8237e65ada6fd11290b7142a4ea220ee2354e235
Ethan Salonga <ethansalonga28@gmail.com>
2022-09-07 17:04:41 -0400

    Add counter functionality

--- src/App.js
+++ src/App.js
@@ -51,10 +51,18 @@ function App() {
     setCart(cart.filter(book => book.id !== item.id))
   }
 
+  function numberOfItems() {
+    let counter = 0
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter
+  }
+
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/books" element={<Books books={books} />} />
