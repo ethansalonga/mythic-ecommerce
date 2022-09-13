262e4a291f6a5bd39248e82d7270df042c0ac9db
Ethan Salonga <ethansalonga28@gmail.com>
2022-09-13 16:51:48 -0400

    Edit cart image/Remove dead assets

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import EmptyCart from "../assets/empty_cart.svg"
+import EmptyCart from "../assets/Amumu.png"
 
 const Cart = ({ cart, changeQuantity, removeItem }) => {
   const total = () => {
